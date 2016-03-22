9b0519d87b5922bea0313f83d4b2cf91340f448d
Pascal Brandt <psbrandt@gmail.com>
2016-03-21 22:23:20 -0700

    ⚓ Move linkify plugin.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Linkify Plugin
+
+*This is a plugin for the `draft-js-plugins-editor`.*
+
+This plugin turns hyperlinks in the text to highlighted, clickable anchor tags!
+
+## Usage
+
+```js
+import createLinkifyPlugin from 'draft-js-linkify-plugin';
+
+const linkifyPlugin = createLinkifyPlugin();
+```
---
+++ src/Link/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import unionClassNames from 'union-class-names';
+import { Map } from 'immutable';
+
+// The component we render when we encouter a hyperlink in the text
+export default class Link extends Component {
+  render() {
+    const { theme = Map(), className, ...props } = this.props; // eslint-disable-line no-use-before-define
+    const combinedClassName = unionClassNames(theme.get('link'), className);
+    return (
+      <a
+        { ...props }
+        href={ this.props.decoratedText }
+        className={ combinedClassName }
+      />
+    );
+  }
+}
